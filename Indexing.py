a5e19f9e18369495c800c963bb80fe807e59ac1c
Anshika Mehta <85496946+anshikamehtaa@users.noreply.github.com>
2022-03-17 21:03:28 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,37 @@
+#Indexing
+value="Norwegian Blue"
+print(value)
+print(value[3])
+print(value[4])
+print()
+print(value[3])
+print(value[6])
+print(value[8])
+
+print("Using negative slicing")
+print(value[-11])
+print(value[-10])
+print()
+print(value[-11])
+print(value[-8])
+print(value[-6])
+
+value="Hakunamatata"
+print(value)
+print(value[2])
+print(value[5])
+print(value[6])
+print(value[7])
+print(value[8])
+
+#Negative slicing
+print("Negative Slicing")
+print(value[-1])
+print(value[-2])
+print(value[-3])
+print(value[-4])
+print(value[-5])
+
+
+
+
