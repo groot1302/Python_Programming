0a5a88d59f8cc76b1909ad45e076d9f2cd10a29b
Anshika Mehta <85496946+anshikamehtaa@users.noreply.github.com>
2023-03-11 21:18:08 +0530

    Update and rename Area to Area.py

--- Area.py
+++ Area.py
@@ -4,7 +4,6 @@ c = float(input('Enter third side: '))
 
 # calculate the semi-perimeter
 s = (a + b + c) / 2
-
-# calculate the area
+# calculate the area 
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+print('The area of the given triangle is %0.2f' %area)
